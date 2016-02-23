0cc8ce3c9fe027c28c87b8fa5ec2a797e97fe050
Robyn Van Deventer <robynv@gridstone.com.au>
2016-02-24 10:08:31 +1100

    Creating project files

---
+++ CFAEvents/CFAEvents/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFAEvents
+//
+//  Created by Robyn Van Deventer on 24/02/2016.
+//  Copyright © 2016 Robyn Van Deventer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
